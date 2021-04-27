eeeb592a89abe1fa68d712b951b5889beb6da705
Rodrigo de Araújo Valt <rodrigo_valt@hotmail.com>
2021-04-26 23:53:18 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/udemy/hrpayroll/resources/PaymentResource.java
@@ -0,0 +1,25 @@
+package com.udemy.hrpayroll.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.udemy.hrpayroll.entities.Payment;
+import com.udemy.hrpayroll.services.PaymentService;
+
+@RestController
+@RequestMapping(value = "/payments")
+public class PaymentResource {
+	
+	@Autowired
+	private PaymentService service;
+
+	@GetMapping(value = "/{workerId}/days/{days}")
+	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
+		return ResponseEntity.ok(service.getPayment(workerId, days));
+	}
+	
+}
