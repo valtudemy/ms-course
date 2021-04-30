7e4d1f799b7e07019d712eb1d2e6338eaa59b131
Rodrigo de Araújo Valt <rodrigo_valt@hotmail.com>
2021-04-29 23:33:47 -0300

    Login, JWT token

---
+++ hr-oauth/src/main/java/com/udemy/hroauth/config/AppConfig.java
@@ -0,0 +1,30 @@
+package com.udemy.hroauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class AppConfig {
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+		accessTokenConverter.setSigningKey("MY-SECRET-KEY");
+		
+		return accessTokenConverter;
+	}
+	
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+
+}
--- hr-oauth/src/main/java/com/udemy/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/udemy/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.udemy.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.udemy.hroauth.entities.User;
 import com.udemy.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
@@ -21,11 +24,16 @@ public class UserService {
 		
 		if(user == null) {
 			logger.error("Email not found " + email );
-			throw new IllegalArgumentException("Email not found");
+			throw new UsernameNotFoundException("Email not found");
 		}
 		
 		logger.info("Email found " + email);
 		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return this.findByEmail(username);
 	}
 
 }
